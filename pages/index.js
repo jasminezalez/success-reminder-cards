029558543b1e36c499aa70a99b815840265c7031
Jasminezalez <jasminezalez@gmail.com>
2023-04-03 18:36:44 -0700

    refactor: rule change

--- pages/index.js
+++ pages/index.js
@@ -20,18 +20,18 @@ const HomePage = () => {
   return (
     <div>
       <select id="dropdown" value={selectedWeek} onChange={handleChange}>
-        <option value="1">Week 1</option>
-        <option value="2">Week 2</option>
-        <option value="3">Week 3</option>
-        <option value="4">Week 4</option>
-        <option value="5">Week 5</option>
-        <option value="6">Week 6</option>
-        <option value="7">Week 7</option>
-        <option value="8">Week 8</option>
-        <option value="9">Week 9</option>
-        <option value="10">Week 10</option>
-        <option value="11">Week 11</option>
-        <option value="12">Week 12</option>
+        <option value="1">Rule 1</option>
+        <option value="2">Rule 2</option>
+        <option value="3">Rule 3</option>
+        <option value="4">Rule 4</option>
+        <option value="5">Rule 5</option>
+        <option value="6">Rule 6</option>
+        <option value="7">Rule 7</option>
+        <option value="8">Rule 8</option>
+        <option value="9">Rule 9</option>
+        <option value="10">Rule 10</option>
+        <option value="11">Rule 11</option>
+        <option value="12">Rule 12</option>
       </select>
       <div id="card-container">
         <div id="card-content">
